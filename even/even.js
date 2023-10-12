63ef2029ee92830ac881985fdbb539a188714dc8
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-12 09:19:24 +0600

    Even and Odd

---
+++ even/even.js
@@ -0,0 +1,14 @@
+// target : 1 to 10 all event numbers
+var number = 0;
+while(number <= 10){
+    // console.log(number);
+    // number++;
+    number = number + 2;
+}
+// -----------------------------------
+// target: 1 to 20 all odd numbers
+var number = 1
+while (number <= 20){
+    console.log(number);
+    number = number + 2;
+}
