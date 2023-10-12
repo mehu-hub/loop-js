75575ad0432988f9313c9a35e4ec44892227c2bc
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-12 23:42:57 +0600

    Array-Loop

---
+++ array-loop/array-loop.js
@@ -0,0 +1,18 @@
+// for( var i = 0; i < 10; i++){
+//     console.log(i);
+// }
+// -----------------------------
+// Target display every element of an Array
+var numbers = [45, 75, 85, 95, 15, 52, 900, 58, 88, 83];
+
+for(var i = 0; i < numbers.length; i++){
+    var number = numbers[i];
+    // console.log(number);
+}
+
+var items = ['Pen', 'Sunglass', 'Book', 'Paper', 'Airpods', 'Mobile', 'Perfume'];
+
+for(var i = 0; i < items.length; i++){
+    var item = items[i];
+    // console.log(item);
+}
