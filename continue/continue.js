dbb070abc8fb85f7793b1f251f92e383837439f3
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-13 00:58:30 +0600

    Continue

---
+++ continue/continue.js
@@ -0,0 +1,11 @@
+var numbers = [45, 75, 85, 95, 15, 52, 900, 58, 23, 83];
+
+for (var i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+
+    if (number > 50) {
+        continue;
+    }
+
+    // console.log(number);
+}
