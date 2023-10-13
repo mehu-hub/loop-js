a2514fd5c906f3e0be70dea3a7b22efca499e563
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-13 10:50:29 +0600

    Reverse

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-<h1>Learn Loop</h1>
+<h1>Topic</h1>
+<hr>
+1. While Loop
+2. For Loop
+3. Break / Continue
+4. Numbers
+5. Even / Odd
+<!-- 6. Array Loop -->
---
+++ reverse/reverse.js
@@ -0,0 +1,17 @@
+var number = 0;
+while(number <= 10){
+    // console.log(number);
+    number++;
+}
+//--------------------------
+// Reverse way of while loop
+var number = 10;
+while(number >= 1){
+    // console.log(number);
+    number--;
+}
+//-------------------------
+// Reverse way of For Lopp
+for(i = 10; i >= 1; i--){
+    console.log(i);
+}
